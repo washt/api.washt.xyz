21c0fb07705dd55266bf05457612be3a310d2990
washt <washt23@gmail.com>
2016-08-01 09:38:41 -0400

    pulled links out into config

--- config.js
+++ config.js
@@ -2,7 +2,12 @@
 
 const config = {
 	baseurl: "http://api.washt.xyz/",
-  port: process.env.PORT 
+  port: process.env.PORT,
+	sites: {
+	"github": "https://api.github.com/users/washt/events",
+  "weather": "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22norfolk%2Cva%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+  "hn": "https://hacker-news.firebaseio.com/v0/user/washt.json?print=pretty"
+	}
 };
 
 module.exports = config;
--- handlers/activity.js
+++ handlers/activity.js
@@ -4,14 +4,12 @@ const Promise = require('bluebird');
 const fetch = require('node-fetch');
 const config = require('../config.js');
 
-
 const activity = () => {
- let github = fetch("https://api.github.com/users/washt/events").then(data => data.json())
+  let github = fetch(config.sites.github).then(data => data.json())
 
- let weather = fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22norfolk%2Cva%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").then(data => data.json())
+  let weather = fetch(config.sites.weather).then(data => data.json())
 
- let hn  = fetch("https://hacker-news.firebaseio.com/v0/user/washt.json?print=pretty")
-             .then(data => data.json())
+  let hn  = fetch(config.sites.hn).then(data => data.json())
 
   return Promise.all([github,weather,hn])
 	  .then(data => {
